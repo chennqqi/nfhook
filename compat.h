c34a017dabc1680f046868c005e9c6bdb0aead9a
JD <johnnie.deacon@gmail.com>
2018-07-27 15:22:50 +0800

    remove unused codes

--- compat.h
+++ compat.h
@@ -8,23 +8,6 @@
 #include <linux/netfilter_arp.h>
 
 #include "nfhook.h"
-#if 0
-// ------------------------------------------------------------------------
-// kernel API compatibility
-
-typedef	int (*PACKET_HANDLER)(int pf, unsigned int hooknum,
-        struct sk_buff *skb, const struct net_device *in,
-        const struct net_device *out);
-
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,3,0)
-#define SUPPORT_NF_REGISTER_NET_HOOK 1
-#endif
-
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,24)
-#define SUPPORT_PERNET_NOTIFIACTION
-typedef	int (*NET_NOTIFYFN_T)(struct net *);
-#endif
-#endif
 
 // ------------------------------------------------------------------------
 // hooking compatibility
